3867c7f6c5c8d657527aab9a10f077b2174598ac
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-08-12 19:39:42 +0000

    changed file format of bg

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -98,7 +98,7 @@ theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/m
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
 --theme.wallpaper = themes_path.."default/background.png"
-theme.wallpaper = "/home/user/.config/awesome/bg/bg.jpg"
+theme.wallpaper = "/home/user/.config/awesome/bg/bg.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
