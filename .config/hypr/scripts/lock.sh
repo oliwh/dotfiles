84bdfd9aabc3c33227a3bab5d4846a5f05fef7e1
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2024-01-25 12:08:11 +0000

    rethemed lock screen

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -2,25 +2,23 @@
 
 swaylock \
     --hide-keyboard-layout \
-    --indicator-radius 100 \
-    --indicator-thickness 7 \
-    --ring-color cba6f7 \
-    --ring-ver-color 89b4fa \
+    --ring-color 313244 \
+    --ring-ver-color cba6f7 \
     --ring-wrong-color f38ba8 \
     --ring-clear-color a6e3a1 \
-    --key-hl-color 1e1e2e \
-    --bs-hl-color eba0ac \
-    --text-color 11111b \
-    --text-caps-lock-color 11111b \
-    --line-color 00000000 \
-    --line-ver-color 00000000 \
-    --line-wrong-color 00000000 \
-    --line-clear-color 00000000 \
-    --separator-color 00000000 \
-    --inside-color cba6f7 \
+    --key-hl-color cba6f7 \
+    --bs-hl-color fab387 \
+    --text-color cdd6f4 \
+    --text-caps-lock-color f38ba8 \
+    --text-wrong-color cdd6f4 \
+    -r \
+    --separator-color 313244
+    --inside-color 313244 \
     --inside-ver-color 89b4fa\
     --inside-wrong-color f38ba8 \
     --inside-clear-color a6e3a1 \
-    --color 1e1e2e80 \
     --clock \
-    --indicator
+    --indicator \
+    --grace-no-mouse \
+    --grace-no-touch \
+    --image ~/.config/hypr/wallpapers/landscape-extended.png \
